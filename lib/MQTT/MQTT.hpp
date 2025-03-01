46bc3b6435b1f50726e4e57f96c7f5f5ac573953
raven1911 <vuduclam1911@gmail.com>
2025-03-01 13:50:18 +0700

    push lam

---
+++ lib/MQTT/MQTT.hpp
@@ -0,0 +1,25 @@
+#ifndef MQTT_HPP
+#define MQTT_HPP
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+//
+
+
+
+
+
+///
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- lib/global_var/global_var.hpp
+++ lib/global_var/global_var.hpp
@@ -12,7 +12,13 @@ extern "C" {
 
 
 
-
+/// Configuration MQTT Server and Wifi Connection
+const char* ssid = "ACLAB";
+const char* password = "ACLAB2023";
+const char* mqtt_server = "test.mosquitto.org";
+// const char* topicSub = "0rm10p/read/Mario@M7";  // Topic nhận dữ liệu
+const char* topicSub = "server/sensors/esp32";  // Topic nhận dữ liệu
+const char* topicPub = "/home/sensors/esp32";         // Topic gửi dữ liệu
 
 
 
