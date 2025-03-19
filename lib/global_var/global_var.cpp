96fc925a47b71a32bce985a35265a5a6a340381f
LAPTOP-6BUUF8U8\MY LE <dammaynholuyenthuyen@gmail.com>
2025-03-19 09:29:39 +0700

    new

--- lib/global_var/global_var.cpp
+++ lib/global_var/global_var.cpp
@@ -1,39 +1,39 @@
 #include <global_var.hpp>
 
-//software timer configuration
+// software timer configuration
 unsigned long millis_present = 0;
-void millis_update(void){
+void millis_update(void)
+{
     millis_present = millis();
 }
 
 /// Configuration MQTT Server and Wifi Connection
-const char* ssid = "ACLAB";
-const char* password = "ACLAB2023";
-const char* mqtt_server = "test.mosquitto.org";
+const char *ssid = "ACLAB";
+const char *password = "ACLAB2023";
+const char *mqtt_server = "test.mosquitto.org";
 
+const char *topicSub = "website/sensors/esp32"; // Topic nhận dữ liệu
+const char *topicPub = "iot/sensors/esp32";     // Topic gửi dữ liệu
 
-const char* topicSub = "website/sensors/esp32";  // Topic nhận dữ liệu
-const char* topicPub = "iot/sensors/esp32";  // Topic gửi dữ liệu
-
-
-deviceName Fan1 = {"Fan1", true, 0};
+deviceName Fan1 = {"Fan1", false, 0};
 deviceName Pump1 = {"Pump1", false, 0};
-deviceName Led1 = {"Led1", true, 0};
+deviceName Led1 = {"Led1", false, 0};
 
-deviceName Fan2 = {"Fan2", true, 0};
+deviceName Fan2 = {"Fan2", false, 0};
 deviceName Pump2 = {"Pump2", false, 0};
-deviceName Led2 = {"Led2", true, 0};
+deviceName Led2 = {"Led2", false, 0};
 
-//biến từ cảm biến
+// biến từ cảm biến
 int Value_SoilMoisture = 0;
 int Value_Light = 0;
 float Value_Temperature = 0.0;
 float Value_Humidity = 0.0;
 bool State_FSM = false;
 
-void pinSetup (void){
+void pinSetup(void)
+{
     pinMode(pump, OUTPUT);
     pinMode(fan, OUTPUT);
     pinMode(soilMoisturePin, INPUT);
-    Wire1.begin(21, 22); //chân của I2C
+    Wire1.begin(21, 22); // chân của I2C
 }
